6281a09e7c3b5235a987882c664db26e3e9c23cf
user <user@WIN-NDAPNIHP71N>
2026-09-24 14:12:39 +0900

    fix：直井 Case.01 commit スクリーンショットの取得追加

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -3,6 +3,10 @@ package jp.co.sss.lms.ct.f01_login1;
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +14,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,11 +41,17 @@ public class Case01 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-		// TODO ここに追加
+	void test01() throws Exception {
+		// // URLでログインページにアクセス
 		webDriver.get("http://localhost:8080/lms");
 
 		assertEquals("http://localhost:8080/lms/", webDriver.getCurrentUrl());
+
+		// 開いたページのキャプチャを取得する
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+
+		//screenshotsフォルダに保存
+		Files.copy(file.toPath(), Paths.get("./src/main/sample01.png"));
 	}
 
 }
