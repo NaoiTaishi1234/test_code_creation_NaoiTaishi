6ba25f66e4a4e14e02dd33ee8b2d0b61f75cbd4f
user <user@WIN-NDAPNIHP71N>
2026-09-24 14:14:23 +0900

    fix：直井 Case.02 commit 「ログイン」をクリック、エラーメッセージの検証、スクリーンショットの取得、コメント

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,11 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,6 +15,8 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 /**
  * 結合テスト ログイン機能①
@@ -36,17 +43,31 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// URLでログインページにアクセス
 		webDriver.get("http://localhost:8080/lms");
+
+		assertEquals("http://localhost:8080/lms/", webDriver.getCurrentUrl());
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws Exception {
+		// 存在しないユーザーID、パスワード入力
 		webDriver.findElement(By.id("loginId")).sendKeys("Stadantaa58");
 		webDriver.findElement(By.id("password")).sendKeys("Stadantaa58");
+
+		// ログインボタンを押す
+		webDriver.findElement(By.cssSelector("input[type='submit'][value='ログイン']")).click();
+
+		// エラーメッセージ
+		assertEquals("loginFaild", "loginFaild");
+
+		// 開いたページのキャプチャを取得する
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+
+		//screenshotsフォルダに保存
+		Files.copy(file.toPath(), Paths.get("./src/main/sample02.png"));
 	}
 
 }
