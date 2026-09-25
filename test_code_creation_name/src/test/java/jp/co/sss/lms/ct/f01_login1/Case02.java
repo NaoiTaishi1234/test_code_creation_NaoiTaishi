8f72b86ce3d1ad06b71e948ff9ed0f8c5c0bc3a8
user <user@WIN-NDAPNIHP71N>
2026-09-25 11:10:01 +0900

    fix：直井 Case.02 commit 検証内容を修正、結合テスト仕様書を更新

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -46,7 +47,7 @@ public class Case02 {
 		// URLでログインページにアクセス
 		webDriver.get("http://localhost:8080/lms");
 
-		assertEquals("http://localhost:8080/lms/", webDriver.getCurrentUrl());
+		assertEquals("ログイン | LMS", webDriver.getTitle());
 	}
 
 	@Test
@@ -61,13 +62,15 @@ public class Case02 {
 		webDriver.findElement(By.cssSelector("input[type='submit'][value='ログイン']")).click();
 
 		// エラーメッセージ
-		assertEquals("loginFaild", "loginFaild");
+		assertEquals("* ログインに失敗しました。",
+				webDriver.findElement(By.cssSelector("span.help-inline.error")).getText());
 
 		// 開いたページのキャプチャを取得する
 		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
 
 		//screenshotsフォルダに保存
-		Files.copy(file.toPath(), Paths.get("./src/main/sample02.png"));
+		Files.copy(file.toPath(), Paths.get("./src/main/sample02.png"),
+				StandardCopyOption.REPLACE_EXISTING);
 	}
 
 }
